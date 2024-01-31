a76f79e08b312972df3b07eb5e06b4f6951cf4eb
marijoyapa <marijoy.apa@tooltwist.com>
2024-01-31 10:19:25 +0800

    store image in Form

--- chat_app/lib/screens/auth.dart
+++ chat_app/lib/screens/auth.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:chat_app/widgets/user_image_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -16,11 +18,12 @@ class _AuthScreenState extends State<AuthScreen> {
   var isLogin = true;
   var _enteredEmail = '';
   var _enteredPassword = '';
+  File? _selectedImage;
 
   void _submit() async {
     var isValid = _form.currentState!.validate();
 
-    if (!isValid) {
+    if (!isValid || !isLogin && _selectedImage == null) {
       return;
     }
     _form.currentState!.save();
@@ -78,7 +81,12 @@ class _AuthScreenState extends State<AuthScreen> {
                       child: Column(
                         mainAxisSize: MainAxisSize.min,
                         children: [
-                          if (!isLogin) const UserImagePicker(),
+                          if (!isLogin)
+                            UserImagePicker(
+                              onPickImage: (pickedImage) {
+                                _selectedImage = pickedImage;
+                              },
+                            ),
                           TextFormField(
                             decoration: const InputDecoration(
                               labelText: 'Email Address',
